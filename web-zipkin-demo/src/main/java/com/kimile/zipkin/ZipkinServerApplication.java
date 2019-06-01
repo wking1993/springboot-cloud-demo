2eef820ac237815928e0bcc4fe1fdadbc29256ea
wangyk <111@com.cn>
2019-06-01 22:33:49 +0800

    整合Zipkin通过RabbitMQ方式收集日志，并将收集到的日志持久化到Elasticsearch中

--- web-zipkin-demo/src/main/java/com/kimile/zipkin/ZipkinServerApplication.java
+++ web-zipkin-demo/src/main/java/com/kimile/zipkin/ZipkinServerApplication.java
@@ -2,11 +2,15 @@ package com.kimile.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+//使用http方式传输sleuth链路数据时使用@EnableZipkinServer注解
+//@EnableZipkinServer
+//通过RabbitMQ方式传输sleuth传输链路数据，使用@EnableZipkinStreamServer注解
+@EnableZipkinStreamServer
 public class ZipkinServerApplication {
 	
 	public static void main(String[] args) {
